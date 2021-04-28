53c4fbf8001ae08b82f006819266d9bdd41d4bb3
Robert Dupplaw <dupplawrobert@gmail.com>
2021-04-28 13:41:01 +0000

    #release_bike now returns a Bike instance

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,9 @@ describe DockingStation do
       subject { DockingStation.new }
         it { is_expected.to respond_to(:release_bike) } 
     end
+
+    describe "#release_bike returns an object that responds #working?" do
+      subject { DockingStation.new.release_bike }
+      it { is_expected.to respond_to(:working?) }
+    end
 end
