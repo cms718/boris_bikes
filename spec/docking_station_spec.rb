4ee00a7b9f746cafc4dce1084e0bbbd405baf115
Charlie Slater <charliemslater1701@gmail.com>
2021-04-28 15:14:24 +0100

    Add docking of bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require "./lib/docking_station.rb"
 
 describe DockingStation do
-    describe ".release_bike" do
+    describe "#release_bike" do
       subject { DockingStation.new }
         it { is_expected.to respond_to(:release_bike) } 
     end
@@ -9,5 +9,19 @@ describe DockingStation do
     describe "#release_bike returns an object that responds #working?" do
       subject { DockingStation.new.release_bike }
       it { is_expected.to respond_to(:working?) }
+    end
+
+    describe "#dock" do
+      subject { DockingStation.new }
+      it { is_expected.to respond_to(:dock).with(1).argument }
+    end
+
+    context "when a bike has been docked" do
+      describe "#bikes returns a bike" do
+        let (:station) { DockingStation.new }
+        let (:bike) { Bike.new }
+        before { station.dock bike }
+        it { expect(station.bike).to eq(bike) }
+      end
     end
 end
