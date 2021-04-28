5c27d3773f546288da217af036fa8108ed02c158
Charlie Slater <charliemslater1701@gmail.com>
2021-04-28 13:09:58 +0100

    Add test to release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require "./lib/docking_station.rb"
 
 describe DockingStation do
+    describe ".release_bike" do
+      it "releases bike" do 
+        docking_station = DockingStation.new
+        expect(docking_station.release_bike).to eq(Bike.new)
+      end
+    end
 end
